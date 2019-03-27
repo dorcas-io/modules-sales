278838d88deba6ec737b7400e1f6fb900f6eee22
Ifeoluwa <bolaji.olawoye@hostvillenigeria.org>
2019-03-27 19:18:56 +0100

    Additional Setup

--- src/ModulesSalesServiceProvider.php
+++ src/ModulesSalesServiceProvider.php
@@ -9,6 +9,12 @@ class ModulesSalesServiceProvider extends ServiceProvider {
 	{
 		$this->loadRoutesFrom(__DIR__.'/routes/web.php');
 		$this->loadViewsFrom(__DIR__.'/resources/views', 'modules-sales');
+		$this->publishes([
+			__DIR__.'/config/modules-sales.php' => config_path('modules-sales.php'),
+		], 'config');
+		$this->publishes([
+			__DIR__.'/assets' => public_path('vendor/modules-sales')
+		], 'public');
 	}
 
 	public function register()
