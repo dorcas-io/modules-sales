236b2c70bdd8c9df129e1493f7fd5ceb9bb62e2c
Olawuyi <olawuyi@Olawuyis-MacBook-Pro.local>
2023-07-11 15:39:44 +0100

    briefly commented out the product-category view breaking category index page

--- src/resources/views/categories.blade.php
+++ src/resources/views/categories.blade.php
@@ -17,7 +17,7 @@
                                   v-bind:show-delete="true" v-on:update="update"
                                   v-on:remove="decrement"></product-category>
             </div>
-            <div class="col s12" v-if="categories.length  === 0">
+            <div class="col s12" v-if="categories.length  ===  0">
                 @component('layouts.blocks.tabler.empty-fullpage')
                     @slot('title')
                         No Product Categories
@@ -59,11 +59,12 @@
                         return axios.post("/msl/sales-categories", {
                             name: value
                         }).then(function (response) {
-                            //console.log(response);
+                            console.log(response);
                             vm.categories.push(response.data);
                             return swal("Success", "The product category was successfully created.", "success");
                         })
                             .catch(function (error) {
+                                console.log(error)
                                 var message = '';
                                 if (error.response) {
                                     // The request was made and the server responded with a status code
@@ -117,126 +118,126 @@
         });
 
 
-Vue.component('product-category', {
-    template: '<div class="col s12">' +
-    '<div class="card">' +
-    '<div class="card-content">' +
-    '<span class="card-title"><h4>{{ category.name }}</h4></span>' +
-    '<p class="flow-text">ID: {{ category.slug }}</p>' +
-    '<p class="flow-text">Product(s): {{ category.products_count }}</p>' +
-    '</div>' +
-    '<div class="card-action">' +
-    '<a href="#" class="grey-text text-darken-3" v-on:click.prevent="edit">Edit</a>' +
-    '<a href="#" class="red-text" v-on:click.prevent="deleteField" v-if="showDelete">REMOVE</a>' +
-    '</div>' +
-    '</div>' +
-    '</div>',
-    props: {
-        category: {
-            type: Object,
-            required: true
-        },
-        index: {
-            type: Number,
-            required: true
-        },
-        showDelete: {
-            type: Boolean,
-            default: false
-        }
-    },
-    data: function () {
-        return {
-            visible: this.seen
-        }
-    },
-    methods: {
-        edit: function () {
-            var context = this;
-            swal({
-                    title: "Update Category",
-                    text: "Enter new name [" + context.category.name + "]:",
-                    type: "input",
-                    showCancelButton: true,
-                    closeOnConfirm: false,
-                    animation: "slide-from-top",
-                    showLoaderOnConfirm: true,
-                    inputPlaceholder: "Custom Field Name"
-                },
-                function(inputValue){
-                    if (inputValue === false) return false;
-                    if (inputValue === "") {
-                        swal.showInputError("You need to write something!");
-                        return false
-                    }
-                    axios.put("/xhr/inventory/categories/"+context.category.id, {
-                        name: inputValue,
-                        update_slug: true
-                    }).then(function (response) {
-                        console.log(response);
-                        context.$emit('update', context.index, response.data);
-                        return swal("Success", "The category name was successfully updated.", "success");
-                    })
-                        .catch(function (error) {
-                            var message = '';
-                            if (error.response) {
-                                // The request was made and the server responded with a status code
-                                // that falls out of the range of 2xx
-                                var e = error.response.data.errors[0];
-                                message = e.title;
-                            } else if (error.request) {
-                                // The request was made but no response was received
-                                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                                // http.ClientRequest in node.js
-                                message = 'The request was made but no response was received';
-                            } else {
-                                // Something happened in setting up the request that triggered an Error
-                                message = error.message;
-                            }
-                            return swal("Oops!", message, "warning");
-                        });
-                });
-        },
-        deleteField: function () {
-            var context = this;
-            swal({
-                title: "Are you sure?",
-                text: "You are about to delete the category (" + context.category.name + ").",
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#DD6B55",
-                confirmButtonText: "Yes, delete it!",
-                closeOnConfirm: false,
-                showLoaderOnConfirm: true
-            }, function() {
-                axios.delete("/xhr/inventory/categories/" + context.category.id)
-                    .then(function (response) {
-                        console.log(response);
-                        context.$emit('remove', context.index);
-                        return swal("Deleted!", "The category was successfully deleted.", "success");
-                    })
-                    .catch(function (error) {
-                        var message = '';
-                        if (error.response) {
-                            // The request was made and the server responded with a status code
-                            // that falls out of the range of 2xx
-                            var e = error.response.data.errors[0];
-                            message = e.title;
-                        } else if (error.request) {
-                            // The request was made but no response was received
-                            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                            // http.ClientRequest in node.js
-                            message = 'The request was made but no response was received';
-                        } else {
-                            // Something happened in setting up the request that triggered an Error
-                            message = error.message;
-                        }
-                        return swal("Delete Failed", message, "warning");
-                    });
-            });
-        }
-    }
-});
+{{--Vue.component('product-category', {--}}
+{{--    template: '<div class="col s12">' +--}}
+{{--    '<div class="card">' +--}}
+{{--    '<div class="card-content">' +--}}
+{{--    '<span class="card-title"><h4>{{ category.name }}</h4></span>' +--}}
+{{--    '<p class="flow-text">ID: {{ category.slug }}</p>' +--}}
+{{--    '<p class="flow-text">Product(s): {{ category.products_count }}</p>' +--}}
+{{--    '</div>' +--}}
+{{--    '<div class="card-action">' +--}}
+{{--    '<a href="#" class="grey-text text-darken-3" v-on:click.prevent="edit">Edit</a>' +--}}
+{{--    '<a href="#" class="red-text" v-on:click.prevent="deleteField" v-if="showDelete">REMOVE</a>' +--}}
+{{--    '</div>' +--}}
+{{--    '</div>' +--}}
+{{--    '</div>',--}}
+{{--    props: {--}}
+{{--        category: {--}}
+{{--            type: Object,--}}
+{{--            required: true--}}
+{{--        },--}}
+{{--        index: {--}}
+{{--            type: Number,--}}
+{{--            required: true--}}
+{{--        },--}}
+{{--        showDelete: {--}}
+{{--            type: Boolean,--}}
+{{--            default: false--}}
+{{--        }--}}
+{{--    },--}}
+{{--    data: function () {--}}
+{{--        return {--}}
+{{--            visible: this.seen--}}
+{{--        }--}}
+{{--    },--}}
+{{--    methods: {--}}
+{{--        edit: function () {--}}
+{{--            var context = this;--}}
+{{--            swal({--}}
+{{--                    title: "Update Category",--}}
+{{--                    text: "Enter new name [" + context.category.name + "]:",--}}
+{{--                    type: "input",--}}
+{{--                    showCancelButton: true,--}}
+{{--                    closeOnConfirm: false,--}}
+{{--                    animation: "slide-from-top",--}}
+{{--                    showLoaderOnConfirm: true,--}}
+{{--                    inputPlaceholder: "Custom Field Name"--}}
+{{--                },--}}
+{{--                function(inputValue){--}}
+{{--                    if (inputValue === false) return false;--}}
+{{--                    if (inputValue === "") {--}}
+{{--                        swal.showInputError("You need to write something!");--}}
+{{--                        return false--}}
+{{--                    }--}}
+{{--                    axios.put("/xhr/inventory/categories/"+context.category.id, {--}}
+{{--                        name: inputValue,--}}
+{{--                        update_slug: true--}}
+{{--                    }).then(function (response) {--}}
+{{--                        console.log(response);--}}
+{{--                        context.$emit('update', context.index, response.data);--}}
+{{--                        return swal("Success", "The category name was successfully updated.", "success");--}}
+{{--                    })--}}
+{{--                        .catch(function (error) {--}}
+{{--                            var message = '';--}}
+{{--                            if (error.response) {--}}
+{{--                                // The request was made and the server responded with a status code--}}
+{{--                                // that falls out of the range of 2xx--}}
+{{--                                var e = error.response.data.errors[0];--}}
+{{--                                message = e.title;--}}
+{{--                            } else if (error.request) {--}}
+{{--                                // The request was made but no response was received--}}
+{{--                                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of--}}
+{{--                                // http.ClientRequest in node.js--}}
+{{--                                message = 'The request was made but no response was received';--}}
+{{--                            } else {--}}
+{{--                                // Something happened in setting up the request that triggered an Error--}}
+{{--                                message = error.message;--}}
+{{--                            }--}}
+{{--                            return swal("Oops!", message, "warning");--}}
+{{--                        });--}}
+{{--                });--}}
+{{--        },--}}
+{{--        deleteField: function () {--}}
+{{--            var context = this;--}}
+{{--            swal({--}}
+{{--                title: "Are you sure?",--}}
+{{--                text: "You are about to delete the category (" + context.category.name + ").",--}}
+{{--                type: "warning",--}}
+{{--                showCancelButton: true,--}}
+{{--                confirmButtonColor: "#DD6B55",--}}
+{{--                confirmButtonText: "Yes, delete it!",--}}
+{{--                closeOnConfirm: false,--}}
+{{--                showLoaderOnConfirm: true--}}
+{{--            }, function() {--}}
+{{--                axios.delete("/xhr/inventory/categories/" + context.category.id)--}}
+{{--                    .then(function (response) {--}}
+{{--                        console.log(response);--}}
+{{--                        context.$emit('remove', context.index);--}}
+{{--                        return swal("Deleted!", "The category was successfully deleted.", "success");--}}
+{{--                    })--}}
+{{--                    .catch(function (error) {--}}
+{{--                        var message = '';--}}
+{{--                        if (error.response) {--}}
+{{--                            // The request was made and the server responded with a status code--}}
+{{--                            // that falls out of the range of 2xx--}}
+{{--                            var e = error.response.data.errors[0];--}}
+{{--                            message = e.title;--}}
+{{--                        } else if (error.request) {--}}
+{{--                            // The request was made but no response was received--}}
+{{--                            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of--}}
+{{--                            // http.ClientRequest in node.js--}}
+{{--                            message = 'The request was made but no response was received';--}}
+{{--                        } else {--}}
+{{--                            // Something happened in setting up the request that triggered an Error--}}
+{{--                            message = error.message;--}}
+{{--                        }--}}
+{{--                        return swal("Delete Failed", message, "warning");--}}
+{{--                    });--}}
+{{--            });--}}
+{{--        }--}}
+{{--    }--}}
+{{--});--}}
 
 
     </script>
